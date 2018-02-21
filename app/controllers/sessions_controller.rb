989649eeb2d2afc1479dc00cc4cd7752551ad632
nakayama <mono649force@yahoo.co.jp>
2018-02-21 13:23:27 +0900

    commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,23 @@
 class SessionsController < ApplicationController
+
   def new
   end
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      #ユーザー情報ページにリダイレクト
       log_in user
-      redirect_to user
+      redirect_to user #ユーザー情報ページにリダイレクト
     else
       #エラーメッセージを作成
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
+
   def destroy
     log_out
     redirect_to root_url
   end
+
 end
